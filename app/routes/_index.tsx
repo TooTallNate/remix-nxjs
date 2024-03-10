9f197a59bde438fa16c5ed2dc5bfd935a1d80b09
Nathan Rajlich <n@n8.io>
2024-03-10 00:41:36 -0800

    Remove "other" route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,27 @@
-import { Link, useLoaderData } from '@remix-run/react';
-import { LoaderFunctionArgs } from '@remix-run/server-runtime';
+import { useLoaderData } from "@remix-run/react";
+import { LoaderFunctionArgs } from "@remix-run/server-runtime";
 
 export async function loader(args: LoaderFunctionArgs) {
-	console.log(args);
-	return { version: Switch.version.nxjs };
+  return { version: Switch.version.nxjs };
 }
 
 export default function Index() {
-	const data = useLoaderData<typeof loader>();
-	return (
-		<div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-			<h1>Welcome to Remix on nx.js v{data.version}</h1>
-			<Link to='/other'>Other</Link>
-		</div>
-	);
+  const data = useLoaderData<typeof loader>();
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Welcome to Remix on nx.js v{data.version}</h1>
+      <ul>
+        <li>
+          <a target="_blank" href="https://nxjs.n8.io" rel="noreferrer">
+            nx.js Docs
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
