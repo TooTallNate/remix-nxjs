fc178ef0003f0a314696da5db2f7183c8eed7933
Nathan Rajlich <n@n8.io>
2024-03-17 17:07:12 -0700

    Cleaner dev logic and print instructions in app

--- dev.js
+++ dev.js
@@ -6,7 +6,14 @@ import express from "express";
 import serveStatic from "serve-static";
 import cors from "cors";
 
-const url = new URL("/__dev", "http://192.168.86.103:8080");
+const baseUrl = process.argv[2];
+if (!baseUrl) {
+    console.log('Error! You must specify the URL of the Switch application!');
+    process.exit(2);
+}
+console.log(`Switch URL: ${baseUrl}`);
+
+const url = new URL("/__dev", baseUrl);
 
 const host = Object.values(networkInterfaces())
   .flatMap((i) => i)
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import * as http from "@nx.js/http";
-import { createRequestHandler } from "@remix-run/server-runtime";
+import {
+  type ServerBuild,
+  createRequestHandler,
+} from "@remix-run/server-runtime";
 
 // @ts-expect-error The Remix server build does not include any types
 import * as build from "../build/server/index.js";
+let serverBuild: ServerBuild = build;
 
 let requestHandler: http.ServerHandler;
 const staticHandler = http.createStaticFileHandler("romfs:/public/");
 
 declare const DEV_MODE: boolean;
 if (DEV_MODE) {
-  (globalThis as any).__devServerBuild = build;
-  requestHandler = (req: Request) =>
-    createRequestHandler((globalThis as any).__devServerBuild)(req);
+  requestHandler = (req: Request) => createRequestHandler(serverBuild)(req);
 } else {
   requestHandler = createRequestHandler(build);
 }
@@ -21,6 +23,17 @@ http.listen({
   port,
 
   async fetch(req) {
+    if (
+      DEV_MODE &&
+      req.method === "POST" &&
+      new URL(req.url).pathname === "/__dev"
+    ) {
+      const module: any = {};
+      new Function("module", await req.text())(module);
+      serverBuild = module.exports;
+      console.log(`Dev server updated: ${new Date()}`);
+      return new Response(null, { status: 202 });
+    }
     let res = await staticHandler(req);
     if (!res) res = await requestHandler(req);
     return res;
@@ -28,4 +41,14 @@ http.listen({
 });
 
 const { ip } = Switch.networkInfo();
-console.log(`HTTP server listening at: http://${ip}:${port}`);
+const url = `http://${ip}:${port}`;
+console.log(`HTTP server listening at: ${url}`);
+
+if (DEV_MODE) {
+  console.log()
+  console.log('\x1B[1m\x1B[33mDEVELOPMENT MODE\x1B[39m\x1B[22m');
+  console.log(`\x1B[33mRun the following command on your local machine:\x1B[39m`);
+  console.log()
+  console.log(`\x1B[36m  npm run dev ${url}\x1B[39m`)
+  console.log()
+}
